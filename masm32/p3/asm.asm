9dfd2919a42200c058aa16cc81b133e5a3f3a46f
DimaGashko <dgashko@mail.ua>
2018-03-18 23:08:37 +0200

    p3

--- masm32/p3/asm.asm
+++ masm32/p3/asm.asm
@@ -1,11 +1,24 @@
 .586
 .model flat, stdcall
 
+extern MessageBoxA@16:near
+
+includelib C:\masm32\lib\user32.lib
+
 data segment
+   head db "Hello, world",0
+   mess db "It's my first asm (almost)",0
+
 data ends
 
 text segment
 start:
-   jmp start
+   push 0
+   push offset head
+   push offset mess
+   push 0
+   call MessageBoxA@16
+   
+   ret
 text ends
 end start
