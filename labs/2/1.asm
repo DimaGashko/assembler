11d4152eec3d876dd056630147223ebbc6f99e24
DimaGashko <dgashko@mail.ua>
2018-04-04 23:27:31 +0300

    2

---
+++ labs/2/1.asm
@@ -0,0 +1,35 @@
+title calc
+;z = 48/x/y + 12*(x-y)
+
+datS segment
+   
+datS ends
+   x db 4 
+   y db 2
+   z dw ?
+codS segment
+assume cs:codS, ds:datS
+
+start: 
+   mov ax, datS
+   mov ds, ax
+   xor ax, ax
+
+   mov ax, 48
+   
+   div x
+   div y 
+   
+   mov z, ax ;48/x/y
+   
+   mov al, 12
+   
+   mov bl, x
+   sub bl, y
+   mul bl
+   
+   add z, ax ;+ 12*(x-y)
+   
+codS ends
+
+end start
